0c8a28507cb826f69a387d6e9cbd7ff56c52817a
Dharvo <adedave77@gmail.com>
2023-05-01 06:05:14 +0000

    modified:   src/Components/Services/Data.js

--- src/Components/Services/Data.js
+++ src/Components/Services/Data.js
@@ -1,2 +1,58 @@
 // SOME DATA FOR SERVICES
+export const LITHIUM__MINING = [
+  {
+    name: "brine",
+    content: [
+      "Lithium brine deposits are accumulations of saline ground water that are enriched in dissolved lithium. ",
+      "Brine can mainly be found in closed basins in arid regions where lithium salts can be extracted. ",
+      "It is pumped to the surface to be evaporated in a succession of ponds.",
+      "Lithium carbonate is the final product of chemical plants that achieve a higher level of purity through transfer across ponds.",
+      "The majority of the lithium made from brines is produced in Chile and Argentina, with a small portion also coming from Nevada in South America.",
+    ],
+  },
+  {
+    name: "hardrock",
+    content: [
+      "Lithium reserves found in ‘hard rock’ are a part of minerals hosted in Pegmatites.",
+      "When mineral-rich magma intrudes from magma chambers into the crust, pegmatites, which are intruding rock units, are created.",
+      "Water and other minerals concentrate when the final portion of this lava cools. ",
+      "The fast development of the massive crystals that set pegmatites apart from other rocks is catalyzed by these fluids rich in metal. ",
+      "Pegmatites can range in size from a few millimeters to hundreds of meters, forming thick seams known as dikes that protrude into neighboring rocks.",
+    ],
+  },
+];
+
+export const PRODUCTS = [
+  {
+    name: "CERAMICS & GLASS",
+    content:
+      "Lithium oxide is widely used as a flux for processing silica reducing the melting point and viscosity of the material and leading to glazes with improved physical properties including low coefficients of thermal expansion.",
+  },
+  {
+    name: "ELECTRICAL & ELECTRONICS",
+    content:
+      "Smaller rechargeable lithium batteries, which are widely used in mobile phones, cameras, and other electronic devices, are one of lithium metal's primary industrial applications.",
+  },
+  {
+    name: "METTALLURGY",
+    content:
+      "Lithium metal's main industrial uses are in metallurgy, where it is utilized as an active element to scavenge from metals including iron, nickel, copper, and zinc as well as their alloys. Lithium scavenges a wide range of non-metallic elements, including oxygen, hydrogen, carbon, and various halogens.",
+  },
+  {
+    name: "PYROTECHNICS",
+    content:
+      "The use of lithium and related compounds in energetic systems includes its use as pyrotechnic colorants and oxidizers in red explosions and flares. ",
+  },
+  {
+    name: "BATTERIES",
+    content:
+      "Smaller rechargeable lithium batteries, which are widely used in mobile phones, cameras, and other electronic devices, are one of lithium metal's primary industrial applications.",
+  },
+  {
+    name: "AIR PURIFICATION",
+    content:
+      "The salts most commonly utilized in restricted environments, such as onboard spacecraft and submarines, for carbon dioxide removal and air purification are lithium hydroxide and lithium peroxide.",
+  },
+];
+
 // SOME DATA FOR PRODUCTS
