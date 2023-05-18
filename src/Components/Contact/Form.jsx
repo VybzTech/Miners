6630cf7349dae5e62b7bd264f65f9756fc0f4825
Dharvo <adedave77@gmail.com>
2023-05-18 10:50:40 +0100

    modified:   src/App.css 	modified:   src/App.scss 	modified:   src/Components/Contact/Form.jsx 	modified:   src/Styles/Contact/Contact.scss

--- src/Components/Contact/Form.jsx
+++ src/Components/Contact/Form.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BsArrowRightShort } from 'react-icons/bs';
 
 const Form = () => {
+    // /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
+    const rgex = ' /^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,}$/i';
+    const [email, setEmail] = useState('');
+    const ChangeEmail = (e) => {
+        email !== e.target.value && setEmail(e.target.value);
+        console.log(email);
+    };
     return (
         <main>
             <div className='Form'>
@@ -12,7 +19,9 @@ const Form = () => {
                         type='email'
                         name='Email'
                         required
-                        id='Email '
+                        id='Email'
+                        value={email}
+                        onChange={ChangeEmail}
                         placeholder='Enter Your Email Address'
                     />
                     <button type='submit'>
