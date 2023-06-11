76d758572a86c05c018a5b6ca5d66aa93ea9962a
Dharvo <adedave77@gmail.com>
2023-06-11 08:12:30 +0100

    modified:   src/App.css 	modified:   src/Components/Contact/Socials.jsx 	modified:   src/Styles/About/About.scss 	modified:   src/Styles/Contact/Contact.scss 	modified:   src/Styles/Contact/Links.scss 	modified:   src/Styles/Home/Lithium.scss 	modified:   src/Styles/Home/Navbar.scss 	modified:   src/Styles/Products/Products.scss 	modified:   src/Styles/Services/Services.scss 	modified:   src/Styles/_colors.scss

--- src/Components/Contact/Socials.jsx
+++ src/Components/Contact/Socials.jsx
@@ -7,9 +7,9 @@ import { MdEmail, MdOutlineWhatsapp } from "react-icons/md";
 
 const Socials = () => {
   const companyEmail = "info@platnumvillemines.com";
-  const tempMsg = "TEMPLATE EMAIL";
+  const tempMsg = "TEMPLATE Message to be Sent !";
   const tempSubject =
-    "Please%20we%20would%20like%20for%Platinum%20Ville%20Mines%20to%20....";
+    "Please%20we%20would%20like%20for%20Platinum%20Ville%20Mines%20to%20....";
   const facebookLink = "";
   const whatsappLink = "08032801376";
   return (
